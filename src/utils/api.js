b17bdb977cfa20ed6495fcfb5a40386123026a9f
Nadeem <mnadeem7556@gmail.com>
2023-05-27 03:20:49 +0530

    error fixed

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,24 +1,20 @@
 import axios from "axios";
 
 const BASE_URL = "https://api.themoviedb.org/3";
-const TMDB_TOKEN = "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIzZjY3YWFiZWZkZDQxZjA2MTBhODYzOTFjZWFjNzI4YiIsInN1YiI6IjY0NmFkNWM5NTRhMDk4MDE3MjhhNDJjMiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.DEtqLyF8x0ldZ94FwAmJYB9f-_u6sjE7Qs35fQgAkJw";
-
+const TMDB_TOKEN = import.meta.env.VITE_APP_TMDB_TOKEN;
 const headers = {
-    Authorization: "bearer " + TMDB_TOKEN,
+  Authorization: "bearer " + TMDB_TOKEN,
 };
 
 export const fetchDataFromApi = async (url, params) => {
-    try{
-        const {data} = await axios.get(BASE_URL + url, {
-            headers,
-            params
-        })
-        return data;
-
-    }
-    catch(error){
-        console.log(error);
-        return error;
-
-}
-}
+  try {
+    const { data } = await axios.get(BASE_URL + url, {
+      headers,
+      params,
+    });
+    return data;
+  } catch (error) {
+    console.log(error);
+    return error;
+  }
+};
